88687241238c0d0f5b007d387bf3c675560f61cc
David Wan <davidwan@davids-mbp.war.can.ibm.com>
2019-10-22 15:03:45 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const { Pool, Client } = require('pg');
+
+app.use(express.static(__dirname + '/build'));
+
+// Pools will look to your environment variables for credentials
+// Make sure you set them as outlined in https://node-postgres.com/features/connecting
+const pool = new Pool();
+
+module.exports.app = app;
